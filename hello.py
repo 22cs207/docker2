5bdc72cacb9f381ea912cea01f8be89c92bb9672
Nandan <1by22cs207@bmsit.in>
2025-06-19 04:34:45 +0530

    making changes

--- hello.py
+++ hello.py
@@ -1,1 +1,9 @@
-print("Hello from Nandan ");
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "🚀 Hello from Docker on localhost!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
